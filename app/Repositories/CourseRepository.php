a1b570cc25fc64c99279793617fbda7411be770c
namdev <namnt10112324@gmail.com>
2025-04-08 16:07:33 +0700

    fix_array_quiz

--- app/Repositories/CourseRepository.php
+++ app/Repositories/CourseRepository.php
@@ -45,29 +45,29 @@ class CourseRepository implements CourseRepositoryInterface
         return DB::table('mdl_course as c')
             ->leftJoin('mdl_context as ctx', function ($join) {
                 $join->on('ctx.instanceid', '=', 'c.id')
-                    ->where('ctx.contextlevel', '=', 50); // Context level của khóa học
+                    ->where('ctx.contextlevel', '=', 50);
             })
             ->leftJoin('mdl_role_assignments as ra', 'ra.contextid', '=', 'ctx.id')
             ->leftJoin('mdl_user as u', function ($join) {
                 $join->on('ra.userid', '=', 'u.id')
-                    ->where('ra.roleid', '=', 3); // Giảng viên (roleid = 3)
+                    ->where('ra.roleid', '=', 3); // Giảng viên
             })
             ->leftJoin('mdl_user as a', function ($join) {
                 $join->on('ra.userid', '=', 'a.id')
-                    ->where('ra.roleid', '=', 4); // Trợ giảng (roleid = 4)
+                    ->where('ra.roleid', '=', 4); // Trợ giảng
             })
             ->leftJoin('mdl_course_categories as cc', 'cc.id', '=', 'c.category')
-            ->join('mdl_user_enrolments as ue', 'ue.courseid', '=', 'c.id') // Join với bảng đăng ký sinh viên
-            ->join('mdl_enrol as e', 'e.id', '=', 'ue.enrolid') // Join với bảng enrol để xác định course của sinh viên
-            ->where('ue.userid', '=', $studentId) // Lọc theo ID của sinh viên
+            ->join('mdl_enrol as e', 'e.courseid', '=', 'c.id')
+            ->join('mdl_user_enrolments as ue', 'ue.enrolid', '=', 'e.id')
+            ->where('ue.userid', '=', $studentId)
             ->select(
                 'c.id as course_id',
                 'c.fullname as course_name',
                 'c.summary as description',
                 'cc.name as category_name',
                 DB::raw('(SELECT COUNT(*) FROM mdl_course_modules cm WHERE cm.course = c.id) AS lesson_count'),
-                DB::raw('(SELECT COUNT(*) FROM mdl_user_enrolments ue 
-                  JOIN mdl_enrol e ON ue.enrolid = e.id WHERE e.courseid = c.id) AS student_count'),
+                DB::raw('(SELECT COUNT(*) FROM mdl_user_enrolments ue2 
+                      JOIN mdl_enrol e2 ON ue2.enrolid = e2.id WHERE e2.courseid = c.id) AS student_count'),
                 DB::raw('GROUP_CONCAT(DISTINCT CONCAT(u.firstname, " ", u.lastname, " (Giáo viên)") SEPARATOR ", ") AS teachers'),
                 DB::raw('GROUP_CONCAT(DISTINCT CONCAT(a.firstname, " ", a.lastname, " (Trợ giảng)") SEPARATOR ", ") AS assistants')
             )
--- routes/api.php
+++ routes/api.php
@@ -27,48 +27,45 @@ use App\Http\Controllers\ForumController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('moodle')->group(function () {
-     // Quên mật khẩu
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-     // Reset mật khẩu
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-    Route::middleware(['auth:sanctum', 'student'])->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/get-user', [AuthController::class, 'getUser']); // lấy ra thông tin tài khoản đang đăng nhập
-        Route::post('/change-password', [AuthController::class, 'changePassword']);
-        // Cập nhật hồ sơ
-        Route::put('/profile', [ProfileController::class, 'updateProfile']);
-        // Thay đổi ngôn ngữ
-        Route::patch('/language', [LanguageController::class, 'updateLanguage']);
+// Quên mật khẩu
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+// Reset mật khẩu
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-        // lấy Danh sách bài tập, trạng thái, section và file bài nộp
-        Route::get('/assignments-questions', [MoodleAssignmentController::class, 'getAssignmentsAndQuestions']);
-        //download file đã nộp
-        Route::get('/download/{contenthash}', [MoodleAssignmentController::class, 'getFile']);
-        //submit bài tập
-        Route::post('/submit-assignment', [MoodleAssignmentController::class, 'submitAssignment']);
 
-        //lấy điểm 
-        Route::get('/grade', [MoodleGradeController::class, 'getStudentGrades']);
+Route::middleware(['auth:sanctum', 'student'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/get-user', [AuthController::class, 'getUser']); // lấy ra thông tin tài khoản đang đăng nhập
+    Route::post('/change-password', [AuthController::class, 'changePassword']);
+    // Cập nhật hồ sơ
+    Route::put('/profile', [ProfileController::class, 'updateProfile']);
+    // Thay đổi ngôn ngữ
+    Route::patch('/language', [LanguageController::class, 'updateLanguage']);
 
-        //lấy câu hỏi hỏi đáp
-        Route::get('/questions', [MoodleQAController::class, 'getQuestions']);
+    // lấy Danh sách bài tập, trạng thái, section và file bài nộp
+    Route::get('/assignment-and-quiz', [MoodleAssignmentController::class, 'getAssignmentsAndQuestions']);
+    //download file đã nộp
+    Route::get('/download/{contenthash}', [MoodleAssignmentController::class, 'getFile']);
+    //submit bài tập
+    Route::post('/submit-assignment', [MoodleAssignmentController::class, 'submitAssignment']);
 
-        //thông tin khóa học
-        Route::get('/course/{course_id}', [CourseInfoController::class, 'getCourseInfo']);
+    //lấy điểm 
+    Route::get('/grade', [MoodleGradeController::class, 'getStudentGrades']);
 
-        Route::get('courses', [CourseController::class, 'getAllCourses']);
-        Route::get('student-courses/{studentId}', [CourseController::class, 'getCoursesByStudentId']);
-        Route::get('student-courses-detail', [CourseController::class, 'getDetailCoursesForStudent']);
+    //lấy câu hỏi hỏi đáp trong thảo luận
+    Route::get('/questions', [MoodleQAController::class, 'getQuestions']); //hỏi đáp trong thảo luận
 
-        //thông tin diễn đàn
-        Route::get('forums', [ForumController::class, 'getForums']); //thông tin diễn đàn mà học sinh đăng nhập
-        Route::get('/forums/{forum_id}/discussions', [ForumController::class, 'getDiscussions']);//lấy ra bài thảo luận trong diễn đàn
-        Route::post('/forums/{forum_id}/discussions', [ForumController::class, 'createDiscussion']); //tạo bài thảo luận mới trong diễn đàn
-        Route::get('/forums/{forum_id}/discussions/{discussion_id}', [ForumController::class, 'getDiscussionDetails']);//lấy ra bài thảo luận trong diễn đàn
-        Route::post('/forums/{forum_id}/discussions/{discussion_id}/comments', [ForumController::class, 'postComment']);//bình luận trong bài thảo luận trong diễn đàn
-    });
-    
+    //thông tin khóa học
+    Route::get('courses', [CourseController::class, 'getAllCourses']);
+    Route::get('student-courses-detail', [CourseController::class, 'getDetailCoursesForStudent']);
+    Route::get('student-courses/{studentId}', [CourseController::class, 'getCoursesByStudentId']);
+
+    //thông tin diễn đàn
+    Route::get('forums', [ForumController::class, 'getForums']); //thông tin diễn đàn mà học sinh đăng nhập
+    Route::get('/forums/{forum_id}/discussions', [ForumController::class, 'getDiscussions']); //lấy ra bài thảo luận trong diễn đàn
+    Route::post('/forums/{forum_id}/discussions', [ForumController::class, 'createDiscussion']); //tạo bài thảo luận mới trong diễn đàn
+    Route::get('/forums/{forum_id}/discussions/{discussion_id}', [ForumController::class, 'getDiscussionDetails']); //lấy ra bài thảo luận trong diễn đàn
+    Route::post('/forums/{forum_id}/discussions/{discussion_id}/comments', [ForumController::class, 'postComment']); //bình luận trong bài thảo luận trong diễn đàn
 });
